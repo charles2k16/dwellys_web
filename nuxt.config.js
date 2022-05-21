8fb6a71e006048a6eb91ab5799f009e6915715e5
DeHerro1 <39437603+DeHerro1@users.noreply.github.com>
2022-05-21 16:35:14 +0000

    icons fixed

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,39 +3,39 @@ export default {
   ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Dwellys',
+    title: "Dwellys",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: 'Discover the world’s hub for properties you would love',
+        hid: "description",
+        name: "description",
+        content: "Discover the world’s hub for properties you would love",
       },
-      { name: 'format-detection', content: 'telephone=023023345' },
+      { name: "format-detection", content: "telephone=023023345" },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    'element-ui/lib/theme-chalk/index.css',
-    '~/assets/css/global.css',
-    '~/assets/css/element.css',
-    '~/assets/css/display.css',
+    "element-ui/lib/theme-chalk/index.css",
+    "~/assets/css/global.css",
+    "~/assets/css/element.css",
+    "~/assets/css/display.css",
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/elementUI/element-ui', '@/plugins/api.js'],
+  plugins: ["@/plugins/elementUI/element-ui", "@/plugins/api.js"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: [
     {
-      path: '~/components', // will get any components nested in let's say /components/test too
+      path: "~/components", // will get any components nested in let's say /components/test too
       pathPrefix: false,
     },
   ],
@@ -43,16 +43,17 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-    '@nuxtjs/auth-next',
+    "@nuxtjs/pwa",
+    "@nuxtjs/auth-next",
+    "nuxt-material-design-icons-iconfont",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -61,7 +62,7 @@ export default {
 
     // https://sg-web-backend.herokuapp.com/api/v1
     // http://localhost:8000/api/v1
-    baseURL: 'http://localhost:8000/api/v1',
+    baseURL: "http://localhost:8000/api/v1",
   },
 
   // router: {
@@ -71,7 +72,7 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en',
+      lang: "en",
     },
   },
 
