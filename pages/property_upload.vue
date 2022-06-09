40d7a7422438b6924678f01275b88b298e360b98
DeHerro1 <39437603+DeHerro1@users.noreply.github.com>
2022-06-09 20:01:13 +0000

    property pages

--- pages/property_upload.vue
+++ pages/property_upload.vue
@@ -5,82 +5,17 @@
         <el-steps :active="active" :space="150" finish-status="success">
           <el-step title="Step 1" description="User information"></el-step>
           <el-step title="Step 2" description="Verification"></el-step>
+          <el-step title="Step 3" description="Specification"></el-step>
+          <el-step title="Step 4" description="Verification"></el-step>
         </el-steps>
       </div>
     </div>
     <div class="section property_upload">
-      <div v-if="step === 1">
-        <div class="center">
-          <div class="property_upload_head">
-            <h4>What kind of property do you want to upload?</h4>
-            <small
-              >Lorem ipsum dolor sit amet, consectetur adipiscing elit.</small
-            >
-          </div>
-          <hr class="hr_rule" />
-        </div>
-        <div class="property_content_container">
-          <div class="property_content">
-            <div v-for="(property, index) in properties" :key="index">
-              <div class="property_main_content" @click="getProperty(property)">
-                <div class="d-flex_column">
-                  <p>
-                    <b> {{ property.title }} </b>
-                  </p>
-                  <p>{{ property.details }}</p>
-                </div>
-                <img :src="getImage(property.img)" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div v-if="step === 2">
-        <div class="center">
-          <div class="property_upload_head">
-            <h4>Provide information on the property specification</h4>
-            <small>Number of bedrooms, bathrooms, garage, kitchen, etc</small>
-          </div>
-          <hr class="hr_rule" />
-        </div>
-        <div class="property_content_container">
-          <div class="property_content">
-            <div v-for="(property, index) in propertiesSpecs" :key="index">
-              <div class="property_main_content" @click="getProperty(property)">
-                <div class="d-flex_column">
-                  <p>
-                    <b> {{ property.title }} </b>
-                  </p>
-                </div>
-                <div class="d-flex">
-                  <p @click="property.no--" class="pr-10">-</p>
-                  {{ property.no }}
-                  <p @click="property.no++" class="pl-10">+</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- <div class="property_upload_btns">
-            <el-button type="info">Back</el-button>
-            <el-button
-              type="primary"
-              class="btn_sm submit_register_button"
-              @click="toNext"
-              >Next</el-button
-            >
-          </div> -->
-        </div>
-      </div>
-      <hr class="hr_rule" />
-      <div class="property_upload_btns pt-10">
-        <el-button type="info" @click="toPrev">Back</el-button>
-        <el-button
-          type="primary"
-          class="btn_sm submit_register_button"
-          @click="toNext"
-          >Next</el-button
-        >
-      </div>
+      <PropertyUploads
+        :properties="properties"
+        :propertiesSpecs="propertiesSpecs"
+        :step="step"
+      />
     </div>
   </div>
 </template>
@@ -100,7 +35,6 @@ export default Vue.extend({
   data() {
     return {
       active: 0 as number,
-      step: 1 as number,
       propertiesSpecs: [
         {
           title: "Bedrooms" as string,
@@ -173,25 +107,17 @@ export default Vue.extend({
     isValid() {},
   },
   methods: {
-    getImage(pic: string): string {
-      return require("../assets/img/" + pic);
-    },
     toPrev(): void {
-      this.step--;
       this.active--;
     },
     toNext(): void {
-      this.step++;
       this.active++;
-    },
-    getProperty(property: Object): void {
-      console.log(property);
     },
   },
 });
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 $small_screen: 426px;
 
 .steps_content {
@@ -239,6 +165,9 @@ $small_screen: 426px;
         padding: 10px;
         border-radius: 12px;
         margin-bottom: 5px;
+        .addSpecs,
+        .reduceSpecs {
+        }
       }
     }
   }
@@ -248,6 +177,10 @@ $small_screen: 426px;
     padding-top: 30px;
     display: flex;
     justify-content: space-between;
+  }
+  .slider {
+    width: 50%;
+    margin: 0 auto;
   }
 }
 </style>
