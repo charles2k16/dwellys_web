b58f93e729d011467fdbe0aee41c301eb5775c48
codehero <awalansarmentor@gmail.com>
2022-06-14 09:24:28 +0000

    completed the property upload ui

---
+++ components/Properties/PlansPricing.vue
@@ -0,0 +1,126 @@
+<template>
+ <div class="plans_price pt-20">
+    <div  v-for="(plan, index) in pricingPlans" :key="index" class="mx-10">
+    <div class="plans" :style="plan.isSelect && {border: '1px solid #de0b0b'}">
+          <h3>{{plan.title}}</h3>
+          <p class="plan_recommend"> {{plan.recommend}} </p>
+          <h4 class="plan_pricing"> {{plan.amount}} </h4>
+          <p class="plan_duration"> {{plan.duration}} </p>
+          <el-button class="plan_btn mb-10"  @click="getPlan(plan)" :type="plan.isSelect && 'primary'" >Select plan</el-button>
+          <div class="pt-20 recommendations">
+            <p class="pb-10"><img src="../../assets/svg/check.png" class="pr-10" />Recommended for individuals</p>
+            <p class="pb-10"><img src="../../assets/svg/check.png" class="pr-10" />Recommended for individuals</p>
+            <p class="pb-10"><img src="../../assets/svg/check.png" class="pr-10" />Recommended for individuals</p>
+            <img src="../../assets/svg/help.png" class="pt-20" />
+          </div>
+        </div>
+    </div>
+ </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+interface property {
+  title: String,
+  recommend: String,
+  amount: String,
+  duration: String,
+  isSelect?: Boolean,
+  }
+
+export default Vue.extend({
+  name: "PlansPricing",
+//   props: {
+//    pricingPlans: {
+//         required: true,
+//         type: Array,
+//     }
+//   },
+  data() {
+    return {
+      
+       pricingPlans: [
+        {
+          title: 'Quick Deals',
+          recommend: 'Recommended for individuals',
+          amount: 'Ghc20.00',
+          duration: 'For 1 month',
+
+          isSelect: true,
+        },
+        {
+          title: 'Quick Sale',
+          recommend: 'Recommended for individuals',
+          amount: 'Ghc20.00',
+          duration: 'For 1 month',
+
+          isSelect: false,
+        },
+        {
+          title: ' Quick Rent',
+          recommend: 'Recommended for individuals',
+          amount: 'Ghc20.00',
+          duration: 'For 1 month',
+
+          isSelect: false,
+        },
+        {
+          title: 'Quick Rent',
+          recommend: 'Recommended for individuals',
+          amount: 'Ghc20.00',
+          duration: 'For 1 month',
+          isSelect: false,
+
+        },
+      ]
+    };
+  },
+  methods: {
+     getPlan(newPlan: property): void {
+   console.log('clicked')
+     this.pricingPlans.filter(plan => plan.title === newPlan.title ? plan.isSelect = true : plan.isSelect = false)
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+$small_screen: 426px;
+.plans {
+    display: flex;
+    flex-direction: column;
+    border: 1px solid #e2e8f0;
+    border-radius: 10px;
+    padding: 25px;
+    text-align: center;
+     @media (max-width: $small_screen) {
+      margin-bottom: 20px;
+      
+     }
+    h3 {
+        padding-bottom: 5px;
+    }
+    .plan_recommend, .plan_duration  {
+        padding-bottom: 30px;
+    }
+    .plan_duration {
+        padding-top: 5px;
+        font-size: 14px;
+    }
+    .plan_pricing {
+        font-weight: 700;
+font-size: 30px;
+line-height: 28px;
+    }
+    .plan_btn {
+
+    }
+    .recommendations {
+        font-size: 14px;
+    }
+
+    
+    
+}
+</style>
--- pages/property_upload.vue
+++ pages/property_upload.vue
@@ -1,21 +1,158 @@
 <template>
   <div>
-    <div class="steps_content">
-      <div class="steps">
-        <el-steps :active="active" :space="150" finish-status="success">
-          <el-step title="Step 1" description="User information"></el-step>
-          <el-step title="Step 2" description="Verification"></el-step>
-          <el-step title="Step 3" description="Specification"></el-step>
-          <el-step title="Step 4" description="Verification"></el-step>
-        </el-steps>
+    <div class="section property_upload">
+      <div v-if="step === 1">
+      <div class="center">
+        <div class="property_upload_head">
+          <h3>What kind of property do you want to upload?</h3>
+          <small
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit.</small
+          >
+        </div>
+        <div class="block slider">
+          <el-slider v-model="step" disabled :min="1" :max="5"></el-slider>
+        </div>
+      </div>
+      <div class="property_content_container">
+        <div class="property_content">
+          <div v-for="(property, index) in properties" :key="index" @click="getProperty(property)" :style="property.isSelect && {background: ' #F1F5F9'}">
+            <div class="property_main_content"  >
+              <div class="d-flex_column">
+                <p>
+                  <b> {{ property.title }} </b>
+                </p>
+                <p>{{ property.details }}</p>
+              </div>
+              <img  :src="getImage(property.img)"/>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="section property_upload">
-      <PropertyUploads
-        :properties="properties"
-        :propertiesSpecs="propertiesSpecs"
-        :step="step"
-      />
+    <div v-if="step === 2">
+      <div class="center">
+        <div class="property_upload_head">
+          <h3>Provide information on the property specification</h3>
+          <small>Number of bedrooms, bathrooms, garage, kitchen, etc</small>
+        </div>
+        <div class="block slider">
+          <el-slider v-model="step" disabled :min="1" :max="5"></el-slider>
+        </div>
+      </div>
+      <div class="property_content_container">
+        <div class="property_content">
+          <div v-for="(property, index) in propertiesSpecs" :key="index">
+            <div class="property_main_content" @click="getProperty(property)">
+              <div class="d-flex_column">
+                <p>
+                  <b> {{ property.title }} </b>
+                </p>
+              </div>
+              <div class="d-flex">
+                  <el-input-number  :min="0" size="small" v-model="property.no"></el-input-number>
+                <!-- <el-button
+                  :disabled="property.no === 0"
+                  @click="property.no--"
+                  class="reduceSpecs"
+                >
+                  -
+                </el-button>
+                <el-button class="d-flex justify_center">{{
+                  property.no
+                }}</el-button>
+                <el-button @click="property.no++" class="addSpecs ml-10"
+                  >+</el-button
+                > -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="step === 3">
+      <div class="center">
+        <div class="property_upload_head">
+          <h3>Select all available amenities</h3>
+          <small>Please check the plausible amenities</small>
+        </div>
+        <div class="block slider">
+          <el-slider v-model="step" disabled :min="1" :max="5"></el-slider>
+        </div>
+      </div>
+      <div class="grid_content_container">
+        <div class="grid_container">
+          <div v-for="(property, index) in amenities" :key="index">
+            <div class="grid_content" :style="property.isSelect && {background: '#E2E8F0'}" @click="getAmenities(property)">
+              <div class="">
+      <img :src="getSvg(property.img)" class="pt-10" />
+    <p  class="mt-30"> {{property.title}} </p>
+    </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="step === 4">
+      <div class="center">
+        <div class="property_upload_head">
+          <h3>Add images of the property</h3>
+          <small>Please check the plausible amenities</small>
+        </div>
+        <div class="block slider">
+          <el-slider v-model="step" disabled :min="1" :max="5"></el-slider>
+        </div>
+      </div>
+      <div class="property_content_container pb-20">
+        <el-upload
+          class="upload-demo"
+          drag
+          action=""
+          :on-preview="handlePreview"
+          :on-remove="handleRemove"
+          :file-list="fileList"
+          multiple
+        >
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">
+            Drop file here or <em>click to upload</em>
+          </div>
+        </el-upload>
+      </div>
+    </div>
+    <div v-if="step === 5">
+      <div class="center">
+        <div class="property_upload_head">
+          <h3>Plans and pricing</h3>
+          <small>Please check the plausible amenities</small>
+        </div>
+        <div class="block slider">
+          <el-slider v-model="step" disabled :min="1" :max="5"></el-slider>
+        </div>
+      </div>
+      <div class="plans_price pb-20">
+        <PlansPricing :pricingPlans="pricingPlans" />
+      </div>
+    </div>
+    <hr class="hr_rule" />
+    <div class="property_upload_btns pt-10">
+      <el-button type="info" @click="toPrev" :disabled="step === 1"
+        >Back</el-button
+      >
+       <el-button
+        type="primary"
+        class="btn_sm submit_register_button"
+        @click="submitUpload"
+        v-if="step === 5"
+        >Submit</el-button
+      >
+      <el-button
+        type="primary"
+        class="btn_sm submit_register_button"
+        @click="toNext"
+        v-else
+        >Next</el-button
+      >
+    </div>
     </div>
   </div>
 </template>
@@ -26,15 +163,24 @@ import VuePhoneNumberInput from "vue-phone-number-input";
 import "vue-phone-number-input/dist/vue-phone-number-input.css";
 import ApplicationHandler from "@/handlers/ApplicationHandler.vue";
 
+ interface property {
+  title: String,
+  details: String,
+  img: String,
+  isSelect: Boolean,
+  }
+
 export default Vue.extend({
   name: "AccountPage",
   components: {
     VuePhoneNumberInput,
     ApplicationHandler,
   },
+ 
   data() {
     return {
-      active: 0 as number,
+      step: 1 as number,
+      fileList: [],
       propertiesSpecs: [
         {
           title: "Bedrooms" as string,
@@ -74,100 +220,210 @@ export default Vue.extend({
           title: "Apartment",
           details: "fitted especially with housekeeping facilities",
           img: "property1.png",
+          isSelect: false,
+
         },
         {
           title: "House",
           details: "fitted especially with housekeeping facilities",
           img: "property2.png",
+        
+          isSelect: false,
         },
         {
           title: "Single room self contain",
           details: "fitted especially with housekeeping facilities",
           img: "property2.png",
+          isSelect: false,
         },
         {
           title: "Single room",
           details: "fitted especially with housekeeping facilities",
           img: "property2.png",
+          isSelect: false,
+          
         },
         {
           title: "Apartment",
           details: "fitted especially with housekeeping facilities",
           img: "property2.png",
+          isSelect: false,
+         
         },
         {
           title: "Apartment",
           details: "fitted especially with housekeeping facilities",
           img: "property1.png",
+          isSelect: false,
         },
       ],
+      amenities: [
+        {
+          img: "ac_unit.png",
+          title: "Air condition",
+          isSelect: false,
+        },
+         {
+          img: "electrical_services.png",
+          title: 'Generator',
+          isSelect: false,
+        },
+         {
+          img: "kitchen.png",
+          title: "Refrigerator",
+          isSelect: false,
+        },
+         {
+          img: "language.png",
+          title: 'Internet',
+          isSelect: false,
+        },
+         {
+          img: "local_laundry_service.png",
+           title: 'Washine machine',
+           isSelect: false,
+        },
+         {
+          img: "mode_fan.png",
+           title: 'Fan',
+           isSelect: false,
+        },
+         {
+          img: "oil_barrel.png",
+           title: 'Water storage',
+           isSelect: false,
+        },
+         {
+          img: "pool.png",
+           title: 'Pool',
+           isSelect: false,
+        },
+         {
+          img: "yard.png",
+          title: "Garden",
+          isSelect: false,
+        }
+      ],
+      pricingPlans: [
+        {
+          title: 'Quick Deals',
+          recommend: 'Recommended for individuals',
+          amount: 'Ghc20.00',
+          duration: 'For 1 month',
+
+          isSelect: false,
+        },
+        {
+          title: 'Quick Sale',
+          recommend: 'Recommended for individuals',
+          amount: 'Ghc20.00',
+          duration: 'For 1 month',
+
+          isSelect: false,
+        },
+        {
+          title: ' Quick Rent',
+          recommend: 'Recommended for individuals',
+          amount: 'Ghc20.00',
+          duration: 'For 1 month',
+
+          isSelect: false,
+        },
+        {
+          title: 'Quick Rent',
+          recommend: 'Recommended for individuals',
+          amount: 'Ghc20.00',
+          duration: 'For 1 month',
+          isSelect: false,
+
+        },
+      ]
     };
   },
   computed: {
     isValid() {},
   },
   methods: {
+    submitUpload() {
+      console.log('submit')
+    },
+     getImage(pic: string): string {
+      return require("../assets/img/" + pic);
+    },
+    getSvg(pic: string): string {
+      return require("../assets/svg/" + pic);
+    },
+    getProperty(newProperty: property): void {
+    //  newProperty.isSelect = true;
+     this.properties.filter(property => property.title === newProperty.title ? property.isSelect = true : property.isSelect = false)
+    },
+     getAmenities(property: property): void {
+      property.isSelect = !property.isSelect;
+    },
     toPrev(): void {
-      this.active--;
+      this.step--;
     },
     toNext(): void {
-      this.active++;
+      this.step++;
     },
   },
 });
 </script>
 
 <style lang="scss">
+
 $small_screen: 426px;
 
-.steps_content {
-  background: #f8fafc;
-  padding: 10px;
-  display: flex;
-  justify-content: flex-end;
-  @media (max-width: $small_screen) {
-    // display: block;
-    justify-content: space-between;
-  }
-  .steps {
-    width: 30%;
-    align-self: flex-end;
-    @media (max-width: $small_screen) {
-      width: 100%;
-      padding: 0 20px;
-    }
-  }
-}
 .property_upload {
-  padding-top: 40px;
+  padding-top: 120px;
   padding-bottom: 50px;
-
+ @media (max-width: $small_screen) {
+      
+      padding: 20px 20px 0;
+    }
   //   margin: 0 auto;
   .property_upload_head {
     padding-bottom: 30px;
-    h4 {
+    h3 {
       font-size: 20px;
       padding-bottom: 5px;
+      line-height: 27px;
+      
+      @media (max-width: $small_screen) {
+      text-align: left;
+    }
+    }
+    small {
+      font-size: 14px;
+      line-height: 20px;
+      color: #64748B;
     }
   }
   .property_content_container {
     padding-top: 20px;
     width: 50%;
     margin: 0 auto;
+    @media (max-width: $small_screen) {
+      width: 100%;
+      margin: 0;
+    }
     .property_content {
       height: 400px;
       overflow-y: scroll;
+      
       .property_main_content {
         display: flex;
         justify-content: space-between;
-        // background: #f8fafc;
         border: 1px solid #e2e8f0;
-        padding: 10px;
+        padding: 15px;
         border-radius: 12px;
-        margin-bottom: 5px;
-        .addSpecs,
-        .reduceSpecs {
+        margin-bottom: 10px;
+        cursor: pointer;
+       @media (max-width: $small_screen) {
+        img {
+          display: none;
         }
+       } 
       }
     }
   }
@@ -177,10 +433,49 @@ $small_screen: 426px;
     padding-top: 30px;
     display: flex;
     justify-content: space-between;
+    @media (max-width: $small_screen) {
+       width: 100%;
+       margin: 0;
+       padding-bottom: 20px;
+       } 
   }
   .slider {
     width: 50%;
     margin: 0 auto;
+  }
+  .grid_content_container {
+    padding-top: 20px;
+    width: 60%;
+    margin: 0 auto;
+    @media (max-width: $small_screen) {
+      width: 100%;
+      margin: 0;
+    }
+  .grid_container {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 20px;
+        margin-bottom: 40px;
+        @media (max-width: $small_screen) {
+      grid-template-columns: repeat(2, 1fr);
+      gap: 10px;
+       } 
+        .grid_content {
+          border: 1px solid #E2E8F0;
+border-radius: 8px;
+text-align: center;
+padding: 20px;
+
+
+        }
+      }
+  }
+  .plans_price {
+    display: flex;
+     @media (max-width: $small_screen) {
+      flex-direction: column;
+      
+     }
   }
 }
 </style>
